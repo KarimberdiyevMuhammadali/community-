1283c55b350c5ac7358704997af3aeea98d1d48c
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-24 01:35:31 +0500

    simple

---
+++ js/main.js
@@ -0,0 +1,49 @@
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+
+const loading = document.getElementById("loading");
+
+const loadingDuration = 2000; // 2s
+
+setTimeout(() => {
+  loading.classList.add('loading-none');
+}, loadingDuration);
